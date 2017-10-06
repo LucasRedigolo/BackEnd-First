c2078c1b9ad72f3e887fcd18e0284f38e146da02
FIC <lucas_redigolo@hotmail.com>
2017-10-05 21:00:37 -0300

    primeira aula

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleCadastro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FazerCafe("caramelo");
+            FazerCafe("chocolate");
+        }
+        static void FazerCafe(string capsula)
+
+        { 
+            
+            System.Console.WriteLine ("Esquentar agua");
+            System.Console.WriteLine ("colocar pó da magia (café)");
+            System.Console.WriteLine ("colocar " + capsula);
+
+        }
+    }
+}
